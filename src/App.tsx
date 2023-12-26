7246a13092d5ecbc43eaa4e2a90fbc2613bff35b
Cristhian Gracia <cristhiangrojas@gmail.com>
2023-12-25 20:02:02 -0500

    markers setups

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Status, Wrapper } from '@googlemaps/react-wrapper';
+import MapComponent from './components/MapComponent';
 
 function App() {
+
+  const render = (status: Status) => (<h1>{status}</h1>)
+
   return (
     <div className="App">
-      
+      <Wrapper apiKey={"AIzaSyB42KE_SyTx0mvEWe1Z4EU9ramLWn4JBF4"} render={render}>
+        <MapComponent />
+      </Wrapper>
     </div>
   );
 }
---
+++ src/components/MapComponent.tsx
@@ -0,0 +1,46 @@
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import { useEffect, useRef, useState } from "react";
+
+export default function MapComponent(){
+  const [map, setMap] = useState<google.maps.Map>()
+  const ref = useRef<HTMLDivElement>()
+  const [markerCluster, setMarkerClusters] = useState<MarkerClusterer>();
+  const [marker, setMarker] = useState<{lat: number, lng: number} | undefined>();
+
+
+  useEffect(()=>{
+    if(ref.current && !map){
+      setMap(new window.google.maps.Map(ref.current, {
+        center: {lat: 4.4333479181711075, lng:-75.21505129646759},
+        zoom: 10,
+      }))
+
+    }
+    if(map && !markerCluster){
+      map.addListener('click', (e: google.maps.MapMouseEvent)=> {
+        if(e.latLng){
+          const {lat, lng} = e.latLng
+          setMarker({lat: lat(), lng: lng()})
+        }
+      })
+      setMarkerClusters(new MarkerClusterer({map, markers: [], }));
+    }
+  }, [map, markerCluster])
+
+  useEffect(()=> {
+    if(marker && markerCluster){
+      markerCluster.addMarker(
+        new window.google.maps.Marker({
+          position: {lat: marker.lat, lng: marker.lng}
+        })
+      )
+    }
+  }, [marker, markerCluster])
+
+  return (
+    
+    <>
+      <div ref={ref as any} style={{height: "100%", width: "700px", minHeight:"700px"}} ></div>
+    </>
+  )
+}

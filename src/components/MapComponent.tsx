19bdc218367adb3aa57e3a35a56864a687c7d2f3
Cristhian Gracia <cristhiangrojas@gmail.com>
2023-12-25 20:03:42 -0500

    changes

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -7,14 +7,12 @@ export default function MapComponent(){
   const [markerCluster, setMarkerClusters] = useState<MarkerClusterer>();
   const [marker, setMarker] = useState<{lat: number, lng: number} | undefined>();
 
-
   useEffect(()=>{
     if(ref.current && !map){
       setMap(new window.google.maps.Map(ref.current, {
         center: {lat: 4.4333479181711075, lng:-75.21505129646759},
         zoom: 10,
       }))
-
     }
     if(map && !markerCluster){
       map.addListener('click', (e: google.maps.MapMouseEvent)=> {
@@ -26,9 +24,10 @@ export default function MapComponent(){
       setMarkerClusters(new MarkerClusterer({map, markers: [], }));
     }
   }, [map, markerCluster])
-
+  
   useEffect(()=> {
     if(marker && markerCluster){
+      markerCluster.clearMarkers();
       markerCluster.addMarker(
         new window.google.maps.Marker({
           position: {lat: marker.lat, lng: marker.lng}
@@ -38,7 +37,6 @@ export default function MapComponent(){
   }, [marker, markerCluster])
 
   return (
-    
     <>
       <div ref={ref as any} style={{height: "100%", width: "700px", minHeight:"700px"}} ></div>
     </>
